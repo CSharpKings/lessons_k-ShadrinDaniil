717421bbd56fa9275d17fc5c05a76a044cfb5ab3
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2017-12-21 18:11:29 +0300

    06

---
+++ 06. if/Весы идеала Шадрин Даниил/Весы идеала Шадрин Даниил/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи свой рост");
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введи свой вес");
+            int weight = int.Parse(Console.ReadLine());
+
+            if (height - 100 == weight){
+                Console.WriteLine("У вас отличная фигура");
+            }
+            if (height - 100 > weight){
+                Console.WriteLine("Вам надо набрать вес");
+            }
+            if (height - 100 < weight)
+            {
+                Console.WriteLine("Вам надо похудеть");
+            }
+                Console.ReadLine();
+               
+        }
+    }
+}
---
+++ 06. if/Дракон и рыцарь/Дракон и рыцарь/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите возраст дракона");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите здоровье дракона");
+            int health = int.Parse(Console.ReadLine());
+            if (age < 150)
+            {
+                Console.WriteLine("В АТАКУ!!!");
+            }
+            else
+            {
+                Console.WriteLine("Не надо... помрешь...");
+            }
+
+            if (health < 50)
+            {
+                Console.WriteLine("В АТАКУ!!!");
+            }
+            else
+            {
+                Console.WriteLine("Не надо... помрешь...");
+            }
+            Console.ReadLine();
+               
+            
+        }
+    }
+}

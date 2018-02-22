2df251926e139e5c9273b07c24904516a0ff5b1e
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-02-22 18:03:01 +0300

    от 20 до 35

---
+++ 12. for/От 20 до 35/От 20 до 35/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            for ( int n = 20; n <= 35; n++)
+            {
+               Console.WriteLine( n );
+            }
+            Console.ReadLine();
+        }
+            
+    }
+}

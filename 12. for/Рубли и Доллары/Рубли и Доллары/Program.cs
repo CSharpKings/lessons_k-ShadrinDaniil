cc2e562fc2bf7f2c2cf56040788f546e16a4fa39
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-02-22 18:02:32 +0300

    Рубли и Доллары

---
+++ 12. for/Рубли и Доллары/Рубли и Доллары/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rouble = 56;
+           
+            for (int dollar = 1; dollar <= 20; dollar++)
+            {
+                 int cash =  dollar * rouble;
+                Console.WriteLine(dollar +"="+ cash );
+                 
+            }
+            Console.ReadLine();
+        }
+    }
+}

eaed1dee4a428f60684a299d5cba4ee52798ff73
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-03-01 18:13:53 +0300

    Солнце и трава с кустами

--- 13. for/LoopyLandscape/Form1.cs
+++ 13. for/LoopyLandscape/Form1.cs
@@ -21,19 +21,24 @@ namespace LoopyLandscape {
 
 			// солнце
 			graphics.FillEllipse(Brushes.Yellow, 310, 20, 70, 70);
+            for (int x = 0; x <= 4; x++)
+            {
+                int coordinates = x * 100;
+                
+                // рисуем дерево и траву
 
-			// рисуем дерево и траву
-			DrawGrass(0, 270);
-			DrawTree(0, 200);
+                DrawGrass(coordinates , 270);
+                DrawTree(coordinates , 200);
+            }
 		}
 
 		private void DrawGrass(int x, int y) {
-			Image img = Image.FromFile("GrassBlock.png");
+            Image img = Image.FromFile("../../GrassBlock.png");
 			graphics.DrawImage(img, x, y);
 		}
 
 		private void DrawTree(int x, int y) {
-			Image img = Image.FromFile("TreeUgly.png");
+			Image img = Image.FromFile("../../TreeUgly.png");
 			graphics.DrawImage(img, x, y);
 		}
 	}

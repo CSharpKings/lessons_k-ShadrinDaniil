56d659e28b131cf1681bbb18baabd1940cb8e883
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-03-15 18:08:28 +0300

    Слово наоборот

---
+++ 13. for/Слово наоборот/Слово наооборот/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ПИШИИИ!");
+            string word = Console.ReadLine();
+          
+
+            for (int n  = word.Length - 1; n >= 0; n--)
+            {
+                Console.WriteLine(word[n]);
+             
+            }
+            Console.ReadLine();
+        }
+    }
+}

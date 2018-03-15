ce6133e6846fc464f8f092e2d2ff537a22e10b27
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-03-15 18:08:59 +0300

    Мне нравится

---
+++ 13. for/Мне нравится/Мне нравится/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи слово");
+            string word = Console.ReadLine();
+            
+            int Last = word.Length - 1;
+            if (word[0] == word[Last])
+            {
+                Console.WriteLine("Мне нравится");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Мне не нравится");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}

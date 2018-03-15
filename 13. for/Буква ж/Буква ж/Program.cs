07735550205343c3880144bea1276b53a8b42bd6
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-03-15 18:08:02 +0300

    Буква ж

---
+++ 13. for/Буква ж/Буква ж/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ПИШИ!");
+            string word = Console.ReadLine();
+           
+            int sum = 0;
+            for (int n = 0; n < word.Length; n++)
+            {
+                if (word[n] == 'ж')
+                {
+                    sum = sum + 1;
+                }
+
+            }
+              Console.WriteLine(sum);
+              Console.ReadLine();
+        }
+    }
+}

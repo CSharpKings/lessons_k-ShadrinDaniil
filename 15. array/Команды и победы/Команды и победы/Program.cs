73cef933ff826e9232d28d68dc6c433effa80207
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-04-05 18:06:56 +0300

    Команды и победы

---
+++ 15. array/Команды и победы/Команды и победы/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] wins = { 4, 2, 6, 1, 9, 3, 4, 0, 2, 19 };
+            for (int team = 0; team < wins.Length; team++)
+            {
+                if (wins[team] < 3)
+                {
+                    Console.WriteLine(team);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}

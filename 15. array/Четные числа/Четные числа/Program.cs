5feeb5af848468ffcbed023066f6687cc40a9ce3
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-04-05 18:07:43 +0300

    Четные числа

---
+++ 15. array/Четные числа/Четные числа/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] integer = {7, 4, 198, 199998, 0, 1, 3, 5, 9, 20};
+           for (int i = 0; i < 10; i++)
+           {
+               if (integer[i] % 2 == 0)
+               {
+                   Console.WriteLine(integer[i]);
+               }
+              
+           }
+                Console.ReadLine();
+        }
+    }
+}

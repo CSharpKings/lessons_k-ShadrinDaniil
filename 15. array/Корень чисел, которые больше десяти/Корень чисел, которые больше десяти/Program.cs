2535250bfebbf0e2f502754501b5ae222e289936
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-04-12 18:21:04 +0300

    Корень чисел, которые < 10

---
+++ 15. array/Корень чисел, которые больше десяти/Корень чисел, которые больше десяти/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = {10, 40, 3, 0, 20000, 6};
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > 10)
+                { 
+                    numbers[i] = Math.Sqrt(numbers[i]);
+                }
+            Console.WriteLine(numbers[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}

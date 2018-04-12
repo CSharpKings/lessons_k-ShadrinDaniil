89b503e2992ef2c212cb61405b964aa0fb586349
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-04-12 18:21:43 +0300

    Числа, которые % 10 = 0

---
+++ 15. array/Числа, которые оканчиваются на 0/Числа, которые оканчиваются на 0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 25, 60, 0, 72, 3000, 40 };
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] % 10 == 0)
+                    {
+                        Console.WriteLine(numbers[i]);
+                    }
+                }
+            Console.ReadLine();
+        }
+    }
+}

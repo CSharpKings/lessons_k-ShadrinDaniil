a7ebf0476f326599ab63f6ab21d5211ed0a43d6e
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-05-10 17:52:13 +0300

    Новая волна каждые 3 секунды, расстояние до дерева

---
+++ 24.unity/Измерить расстояние до объекта/Assets/scripts/MeasureTheDistance.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeasureTheDistance : MonoBehaviour {
+	public Transform enemy;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance(enemy.position, transform.position);
+		print (dist);
+
+	}
+}
---
+++ Lesson 2/Assets/Scripts/Write.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Write : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	int number = 0;
+	float time = 0;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		time = time + Time.deltaTime;
+
+		if (time >= 3) {
+			time = 0;
+			number = number + 1;
+			print ("Монстры атакуют волна" + " " + number);
+		}
+
+	}
+}

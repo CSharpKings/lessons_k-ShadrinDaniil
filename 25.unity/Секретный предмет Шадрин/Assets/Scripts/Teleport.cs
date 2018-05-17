a6201bff1b2fa7263b5a3c6493c0cb38c6708b88
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-05-17 17:56:51 +0300

    Игра Найди предмет

---
+++ 25.unity/Секретный предмет Шадрин/Assets/Scripts/Teleport.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+	float time = 0;
+	// Update is called once per frame
+	void Update ()
+	{
+
+		time = time + Time.deltaTime;
+		if (time > 20) {
+			transform.position = new Vector3 (-5.577394f, 2.040581f, -2.73927f);
+			time = 0;
+		}
+
+	}
+}
---
+++ 25.unity/Секретный предмет Шадрин/Assets/Scripts/TextWin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextWin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	public Transform cube;
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance (cube.position, transform.position);
+		if (dist < 3) {
+			print ("Победа!");
+		}
+
+
+	}
+}

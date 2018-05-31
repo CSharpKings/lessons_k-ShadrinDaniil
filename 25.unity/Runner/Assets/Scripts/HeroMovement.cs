def89e5c8260732f50df8622eb1e49add08a3aea
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-05-31 21:05:07 +0300

    Раннер

---
+++ 25.unity/Runner/Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		speed = GetComponent <Speed> ();
+	}
+	Speed speed;
+	float time = 0;
+	// Update is called once per frame
+	void Update ()
+	{
+		time = time + Time.deltaTime;
+		if (Input.GetKeyDown (KeyCode.Space)||Input.GetKeyDown (KeyCode.Mouse0)) {
+			transform.position = new Vector3 (transform.position.x, 2.2f, transform.position.z);
+		}
+		if (time >= 1) {
+			speed.enabled = true;
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		transform.position = new Vector3 (0f, 0.424f, 0f); 
+		
+		speed.enabled = false;
+		time = 0;
+	}
+}
+
+	
--- 25.unity/Runner/Assets/Scripts/Speed.cs
+++ 25.unity/Runner/Assets/Scripts/Speed.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControl : MonoBehaviour {
+public class Speed : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +11,9 @@ public class CameraControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (transform.position.y <= 0.424){
+			Rigidbody body = GetComponent<Rigidbody>();
+			body.velocity = new Vector3(-6, 0, 0);
+		}
 	}
 }
